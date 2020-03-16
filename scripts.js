e0eb81c23d3e92895795a4fc31d564cd8e0d6889
TryToCompile <43519371+TryToCompile@users.noreply.github.com>
2020-03-16 16:47:38 +0300

    feat: add form script

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
 const MENU = document.getElementById('menu');
+const BUTTON = document.getElementById('btn');
+const CLOSE_BUTTON = document.getElementById('close-btn');
 
 MENU.addEventListener('click', (event) => {
    MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
 
+BUTTON.addEventListener('click', () => {
+    const subject = document.getElementById('subject').value.toString();
+    const description = document.getElementById('description').value.toString();
+    document.getElementById('result').innerText = 'The letter was sent';
+    document.getElementById('pop-theme').innerText = 'Subject: ' + subject;
+    document.getElementById('pop-description').innerText = 'Description: ' + description;
+    document.getElementById('message-block').classList.remove('hidden');
+    document.getElementById('message').classList.remove('hidden');
+});
+
+CLOSE_BUTTON.addEventListener('click', () => {
+    document.getElementById('result').innerText = "";
+    document.getElementById('pop-description').innerText = "";
+    document.getElementById('message-block').classList.add('hidden');
+    document.getElementById('message').classList.add('hidden');
+    /*this.form.reset();*/
+});
