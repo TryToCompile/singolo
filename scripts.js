475212b096c1b2e3568f240a26ebb57afe3c35d9
TryToCompile <43519371+TryToCompile@users.noreply.github.com>
2020-03-18 16:11:50 +0300

    feat: add slider features

--- scripts.js
+++ scripts.js
@@ -1,27 +1,103 @@
 const MENU = document.getElementById('menu');
 const BUTTON = document.getElementById('btn');
 const CLOSE_BUTTON = document.getElementById('close-btn');
+const FORM = document.getElementById('forma');
+const CONTACT_FORM_NAME_REQUIRED = document.getElementById('req1');
+const CONTACT_FORM_EMAIL_REQUIRED = document.getElementById('req2');
+const CONTAINER = document.getElementById('container');
+const BTN_HOR = document.querySelector('.container-btn-hor');
+const BTN_VERT = document.querySelector('.container-btn-vert');
+const SCREEN_HOR = document.querySelector('.container-screen-hor');
+const SCREEN_VERT = document.querySelector('.container-screen-vert');
 
-MENU.addEventListener('click', (event) => {
-   MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+
+let click_button_vert = 1;
+let click_button_hor = 1;
+/////////////////////////////////////// SLIDER BUTTONS ////////////////////////////////
+
+BTN_VERT.addEventListener('click', () => {
+    if( click_button_vert%2 == 0) {
+        SCREEN_VERT.style.cssText = 'opacity:0%';
+         BTN_VERT.style.cssText = 'opacity:100%';
+        click_button_vert++;
+    } else {
+        SCREEN_VERT.style.cssText = 'opacity:100%';
+        BTN_VERT.style.cssText = 'opacity:0%';
+        click_button_vert++;
+    }
+});
+
+
+BTN_HOR.addEventListener('click', () => {
+    if( click_button_hor%2 == 0) {
+        SCREEN_HOR.style.cssText = 'opacity:0%';
+        BTN_HOR.style.cssText = 'opacity:100%';
+        click_button_hor++;
+    } else {
+        SCREEN_HOR.style.cssText = 'opacity:100%';
+         BTN_HOR.style.cssText = 'opacity:0%';
+        click_button_hor++;
+    }
+});
+
+SCREEN_HOR.addEventListener('click', () => {
+    if( click_button_hor % 2 == 0) {
+        SCREEN_HOR.style.cssText = 'opacity:0%';
+        click_button_hor++;
+    } else {
+        SCREEN_HOR.style.cssText = 'opacity:100%';
+        click_button_hor++;
+    }
+});
+
+SCREEN_VERT.addEventListener('click', () => {
+    if( click_button_vert % 2 == 0) {
+        SCREEN_VERT.style.cssText = 'opacity:0%';
+        click_button_vert++;
+    } else {
+        SCREEN_VERT.style.cssText = 'opacity:100%';
+        click_button_vert++;
+    }
+});
+if (SCREEN_VERT.style.opacity === '0%') {
+    console.log('lalala');
+}
+
+
+
+/*
+window.addEventListener('scroll', () => {
+    CONTAINER.querySelectorAll('section').forEach(el => 
+console.log(el.offsetTop, MENU.scrollTop, el.scrollHeight), Elementr);
+});
+*/
+
+
+MENU.addEventListener('click', () => {
+    MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
 
 BUTTON.addEventListener('click', () => {
-    const subject = document.getElementById('subject').value.toString();
-    const description = document.getElementById('description').value.toString();
-    document.getElementById('result').innerText = 'The letter was sent';
-    document.getElementById('pop-theme').innerText = 'Subject: ' + subject;
-    document.getElementById('pop-description').innerText = 'Description: ' + description;
-     if (subject == "") {
-        document.getElementById('pop-theme').innerText = 'Without subject';
+    let name = CONTACT_FORM_NAME_REQUIRED.validity.valid;
+    let email = CONTACT_FORM_EMAIL_REQUIRED.validity.valid;
+    if (name == true && email == true) {
+        event.preventDefault();
+        const subject = document.getElementById('subject').value.toString();
+        const description = document.getElementById('description').value.toString();
+        document.getElementById('result').innerText = 'The letter was sent';
+        document.getElementById('pop-theme').innerText = 'Subject: ' + subject;
+        document.getElementById('pop-description').innerText = 'Description: ' + description;
+        if (subject == "") {
+            document.getElementById('pop-theme').innerText = 'Without subject';
+        }
+
+        if (description == "") {
+            document.getElementById('pop-description').innerText = 'Without description';
+        }
+        document.getElementById('message-block').classList.remove('hidden');
+        document.getElementById('message').classList.remove('hidden');
     }
-    
-    if (description == "") {
-        document.getElementById('pop-description').innerText = 'Without description';
-    }
-    document.getElementById('message-block').classList.remove('hidden');
-    document.getElementById('message').classList.remove('hidden');
 });
 
 CLOSE_BUTTON.addEventListener('click', () => {
@@ -29,5 +105,7 @@ CLOSE_BUTTON.addEventListener('click', () => {
     document.getElementById('pop-description').innerText = "";
     document.getElementById('message-block').classList.add('hidden');
     document.getElementById('message').classList.add('hidden');
-    /*this.form.reset();*/
+    FORM.reset();
 });
+
+
